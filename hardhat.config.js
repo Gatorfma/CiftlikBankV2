2575f5916f6e0ac455124b372f376ca0564ee8e9
Furkan Mert Aksakal <mert.aksakal@ug.bilkent.edu.tr>
2024-08-14 17:25:47 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     sepolia: {
+      url: 'https://sepolia.infura.io/v3/5bb1b0ecbcda40fd892d4914faa49105',
+      chainid: 11155111,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
